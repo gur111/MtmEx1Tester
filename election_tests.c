8e83c813641da6dbed55c6ba67dd673903763935
Gur Telem <gur111telem@gmail.com>
2020-04-27 16:00:29 -0700

    Uncomment FORK

--- election_tests.c
+++ election_tests.c
@@ -10,7 +10,7 @@
 #define STRESS_INVERTALS_MODIFIER 10000
 
 #ifdef __unix__
-// #define WITH_FORK
+#define WITH_FORK
 // Fuck Microsoft and all it stands for.
 // If you need to debug on this shitty OS, get the errors one by one.
 // Also, good luck. You'll need it
