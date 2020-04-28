89e02487205bc1ce73437ff496928c8dd76a5c6e
danibondar <danibondar@gmail.com>
2020-04-28 12:19:44 +0300

    - added tests for AddVotes function

--- election_tests.c
+++ election_tests.c
@@ -22,7 +22,7 @@
 #endif
 
 #ifdef __MACH__
-#define WITH_FORK
+//#define WITH_FORK
 // Fuck Microsoft and all it stands for.
 // If you need to debug on this shitty OS, get the errors one by one.
 // Also, good luck. You'll need it
